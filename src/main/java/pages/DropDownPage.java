4c2871b28f2ed3dda9625f376b7a70da9b7265ea
momtazrasel <momtazrasel8@gmail.com>
2022-09-05 20:09:44 +0600

    dropdown page added

---
+++ src/main/java/pages/DropDownPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DropDownPage {
+
+    private WebDriver driver;
+    private By dropdown = By.id("dropdown");
+    public DropDownPage(WebDriver driver){
+        this.driver = driver;
+    }
+    public void selectFromDropDown(String option){
+        findDropDownElement().selectByVisibleText(option);
+
+    }
+    public List<String> getSelectedOption(){
+        List<WebElement> selectElements = findDropDownElement().getAllSelectedOptions();
+        return selectElements.stream().map(e->e.getText()).collect(Collectors.toList());
+    }
+    private Select findDropDownElement(){
+        return new Select(driver.findElement(dropdown));
+    }
+}
--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -5,13 +5,19 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     private final WebDriver driver;
-    private By formAuthenticationLink = new By.ByLinkText("Form Authentication");
 
     public HomePage(WebDriver driver){
         this.driver = driver;
     }
     public LoginPage clickFormAuthentication(){
-        driver.findElement(formAuthenticationLink).click();
+        clickLink("formAuthenticationLink");
         return new LoginPage(driver);
+    }
+    public DropDownPage clickDropdown(){
+        clickLink("Dropdown");
+        return new DropDownPage(driver);
+    }
+    public void clickLink(String linkText){
+        driver.findElement(By.linkText((linkText))).click();
     }
 }
